ed9c9a5eada5bdce181398bb6f06f54eba998fbc
Steven Fackler <sfackler@gmail.com>
2015-05-16 22:05:59 +0000

    rebuild pages at ee5d171a07dc1c07171f7ea70c3fa1286e151a60

---
+++ doc/master/implementors/core/iter/trait.IntoIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['postgres'] = ["impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.IntoIterator.html' title='core::iter::IntoIterator'>IntoIterator</a> for &amp;'a <a class='struct' href='postgres/struct.Rows.html' title='postgres::Rows'>Rows</a>&lt;'a&gt;","impl&lt;'stmt&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.IntoIterator.html' title='core::iter::IntoIterator'>IntoIterator</a> for <a class='struct' href='postgres/struct.Rows.html' title='postgres::Rows'>Rows</a>&lt;'stmt&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
