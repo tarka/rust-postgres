66ba6e1bcd252e63f1b8d1c2c41d4ab35dc0848c
Steven Fackler <sfackler@gmail.com>
2015-05-18 03:33:45 +0000

    rebuild pages at edb990d20fd235f72fd33248fd3a56821c3007cf

--- doc/master/implementors/std/io/trait.Write.js
+++ doc/master/implementors/std/io/trait.Write.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['postgres'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Write.html' title='std::io::Write'>Write</a> for <a class='struct' href='postgres/io/struct.Stream.html' title='postgres::io::Stream'>Stream</a>",];
+implementors['postgres'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Write.html' title='std::io::Write'>Write</a> for <a class='struct' href='postgres/io/struct.Stream.html' title='postgres::io::Stream'>Stream</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- doc/master/implementors/std/sys/ext/io/trait.AsRawFd.js
+++ doc/master/implementors/std/sys/ext/io/trait.AsRawFd.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['postgres'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/sys/ext/io/trait.AsRawFd.html' title='std::sys::ext::io::AsRawFd'>AsRawFd</a> for <a class='struct' href='postgres/io/struct.Stream.html' title='postgres::io::Stream'>Stream</a>",];
+implementors['postgres'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/sys/ext/io/trait.AsRawFd.html' title='std::sys::ext::io::AsRawFd'>AsRawFd</a> for <a class='struct' href='postgres/io/struct.Stream.html' title='postgres::io::Stream'>Stream</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
