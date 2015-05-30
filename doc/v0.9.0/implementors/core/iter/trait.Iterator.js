7ffa0d6d1a3f4f0c1e6c67fe5483bd06a2cd908e
Steven Fackler <sfackler@gmail.com>
2015-05-29 23:24:06 -0700

    Release v0.9.0

---
+++ doc/v0.9.0/implementors/core/iter/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['postgres'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='postgres/rows/struct.Iter.html' title='postgres::rows::Iter'>Iter</a>&lt;'a&gt;","<a class='stability Stable' title='Stable'></a>impl&lt;'stmt&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='postgres/rows/struct.IntoIter.html' title='postgres::rows::IntoIter'>IntoIter</a>&lt;'stmt&gt;","<a class='stability Stable' title='Stable'></a>impl&lt;'trans, 'stmt&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='postgres/rows/struct.LazyRows.html' title='postgres::rows::LazyRows'>LazyRows</a>&lt;'trans, 'stmt&gt;","<a class='stability Stable' title='Stable'></a>impl&lt;'conn&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='postgres/struct.Notifications.html' title='postgres::Notifications'>Notifications</a>&lt;'conn&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
